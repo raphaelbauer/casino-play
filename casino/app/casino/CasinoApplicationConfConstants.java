c1577904dffadaa2dff29d812a7aac74ace0be0d
reyez <raphael.andre.bauer@gmail.com>
2012-01-21 02:14:08 +0100

    tiny fix

---
+++ casino/app/casino/CasinoApplicationConfConstants.java
@@ -0,0 +1,19 @@
+package casino;
+
+/**
+ * Used in application.conf
+ * @author ra
+ *
+ */
+public interface CasinoApplicationConfConstants {
+	
+	/* must be set in application conf */
+	public static String EMAIL_FROM = "casino.emailFrom";
+	
+	/* optional in application conf */
+	public static String SECURE_URL = "casino.secureUrl";
+	
+	/* optional in application conf */
+	public static String REGULAR_URL = "casino.regularUrl";
+
+}
